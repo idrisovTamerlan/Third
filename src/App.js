6021c061e64cbb9580d0181e0dc30c49e4d9f101
Tamerlan <idrisovtamerlan@ya.ru>
2021-04-23 17:27:09 +0300

    Download

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import surt from './surt.jpg';
+
+function App() {
+    return (
+        <div>
+            <h1> Привет, это Че Гевара </h1>
+            <img src={surt}/>
+        </div>
+    );
+}
+
+export default App;
